92e022ab2efbc632aa77ea1a48d8b2bd05b3137f
Duc Viet Hoang <vietyb00@gmail.com>
2023-02-25 15:19:59 +0700

    hyprrrrrr land

---
+++ hypr/swaylock.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 150 \
+	--indicator-thickness 10 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+  --font "CaskaydiaCove Nerd Font" \
+	--ring-color ebdbb2 \
+	--key-hl-color 8ec07c \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+  --inside-ver-color 83a598 \
+  --ring-ver-color 458588 \
+  --inside-wrong-color fb4934 \
+  --ring-wrong-color cc241d \
+  --text-wrong-color fe8019 \
+  --text-color fabd2f \
+	--separator-color 00000000 \
+	--grace 2 \
+	--fade-in 0.2

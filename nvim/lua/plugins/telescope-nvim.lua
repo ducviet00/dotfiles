a7b0aa0eb4b16130e72b985d4cdcf8bff22cc98d
ducviet00 <viet.hoang@moreh.com.vn>
2024-05-29 10:49:09 +0700

    small update and nvim

---
+++ nvim/lua/plugins/telescope-nvim.lua
@@ -0,0 +1,32 @@
+-- Fuzzy finder
+return {
+  -- https://github.com/nvim-telescope/telescope.nvim
+  'nvim-telescope/telescope.nvim',
+  lazy = true,
+  dependencies = {
+    -- https://github.com/nvim-lua/plenary.nvim
+    { 'nvim-lua/plenary.nvim' },
+    {
+      -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
+  },
+  opts = {
+    defaults = {
+      layout_config = {
+        vertical = {
+          width = 0.75
+        }
+      },
+      path_display = {
+        filename_first = {
+          reverse_directories = true
+        }
+      },
+    }
+  }
+}
